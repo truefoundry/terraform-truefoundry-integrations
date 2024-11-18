2444913b63f6ffd27255019b7e9ebcd2e02e1f92
Harshit Luthra <love.coolsachin@gmail.com>
2024-11-18 13:52:11 +0530

    update changes

--- main.tf
+++ main.tf
@@ -1,18 +1,8 @@
 resource "null_resource" "create_cluster" {
   triggers = {
-<<<<<<< HEAD
     cluster_name  = var.cluster_name
     cluster_type  = var.cluster_type
     always_update = var.always_update ? timestamp() : "initial"
-||||||| parent of fe521d0 (update trigger update variable)
-    cluster_name        = var.cluster_name
-    cluster_type        = var.cluster_type
-    trigger_helm_update = var.trigger_helm_update ? timestamp() : "initial"
-=======
-    cluster_name = var.cluster_name
-    cluster_type = var.cluster_type
-    always_apply = var.always_apply ? timestamp() : "initial"
->>>>>>> fe521d0 (update trigger update variable)
   }
 
   provisioner "local-exec" {
