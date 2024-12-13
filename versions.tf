e225aedcb11fe70df64da8470ac55fe9edd32bf5
Vedant Pareek <dunefro@gmail.com>
2024-12-13 20:50:45 +0530

    Corrected required providers

--- versions.tf
+++ versions.tf
@@ -1,13 +1,9 @@
 terraform {
   required_version = "~> 1.4"
   required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5"
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.4"
     }
   }
 }
